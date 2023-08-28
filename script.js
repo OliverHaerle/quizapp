ad62a52391fcf55eb56339ba08a8c75b7cc0b3f6
Oliver Härle <Oliver.haerle@outlook.com>
2023-08-28 11:54:15 +0200

    created HTML code

---
+++ script.js
@@ -0,0 +1,54 @@
+let questions = [
+    {
+        "question": "Who invented HTML?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Homer Simpson",
+        "right_answer": 3
+    },
+    {
+        "question": "What is the name of the alien duo who are often seen in the series?",
+        "answer_1": "Bort and Cort",
+        "answer_2": "Zog and Blip",
+        "answer_3": "Xur and Yar",
+        "answer_4": "Kodos and Kang",
+        "right_answer": 4
+    },
+    {
+        "question": "In 'Lisa the Vegetarian' (Season 7, Episode 5), who helps Lisa embrace her vegetarian lifestyle?",
+        "answer_1": "Paul McCartney",
+        "answer_2": "John Lennon",
+        "answer_3": "Ringo Starr",
+        "answer_4": "George Harrison",
+        "right_answer": 1
+    },
+    {
+        "question": "In 'The Itchy & Scratchy & Poochie Show' (Season 8, Episode 14), which character is added to the Itchy & Scratchy cartoon show to boost its ratings?",
+        "answer_1": "Roy",
+        "answer_2": "Poochie",
+        "answer_3": " Scratchy Jr.",
+        "answer_4": "Snappy",
+        "right_answer": 2
+    },
+    {
+        "question": "What business does Homer start after his car is destroyed in a snowstorm?",
+        "answer_1": "Snow shoveling service",
+        "answer_2": "Ice cream truck",
+        "answer_3": "Snow cone stand",
+        "answer_4": "Snowmobile rental",
+        "right_answer": 1
+    },
+    {
+        "question": "In 'Radioactive Man' (Season 7, Episode 2), which role does Milhouse land in the film adaptation of the Radioactive Man comic?",
+        "answer_1": "Radioactive Man",
+        "answer_2": "Fallout Boy",
+        "answer_3": "Dr. Crab",
+        "answer_4": " Captain Squid",
+        "right_answer": 2
+    },
+];
+
+function onload() {
+    
+}
