7978dbfe42bd76cec4a5db8a11f6092ae70c24a5
Oliver Härle <Oliver.haerle@outlook.com>
2023-08-28 17:07:29 +0200

    created HTML code

--- script.js
+++ script.js
@@ -49,6 +49,35 @@ let questions = [
     },
 ];
 
-function onload() {
-    
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('total-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('question-text').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+   let question = questions[currentQuestion];
+   let selectedQuestionNumber = selection.slice(-1);
+
+
+    let idOfRightAnswer = question[`right_answer`];
+
+   if (selectedQuestionNumber == question['right_answer']) {
+    document.getElementById(selection).parentNode.classList.add('bg-success');
+   } else {
+    document.getElementById(selection).parentNode.classList.add('bg-danger');
+    document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+   }
 }
